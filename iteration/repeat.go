28fb9f5835e5cd2dd218f86194d6984386477257
lotadcate <lotadcate@gmail.com>
2022-12-17 11:04:35 +0900

    The number of for statements was specified in the argument.

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,9 +1,9 @@
 package iteration
 
-const repeatCount = 5
-func Repeat(c string) string {
+// const repeatCount = 5
+func Repeat(c string, count int) string {
   var repeated string
-  for i:=0; i<repeatCount; i++ {
+  for i:=0; i<count; i++ {
     repeated += c
   }
   return repeated
