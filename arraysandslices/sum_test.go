cf50d93513eba9d369d1428e3245b8a5b261c916
lotadcate <lotadcate@gmail.com>
2022-12-17 11:35:30 +0900

    Array could support array, but compilation error in slice.

--- arraysandslices/sum_test.go
+++ arraysandslices/sum_test.go
@@ -2,10 +2,24 @@ package arrayandslices
 import "testing"
 
 func TestSum(t *testing.T) {
-  numbers := [5]int{1, 2, 3, 4, 5}
-  got := Sum(numbers)
-  want := 15
-  if got != want {
-    t.Errorf("got %d want %d given, %v", got, want, numbers)
+  assert := func(t testing.TB, got, want int, arr [5]int) {
+    t.Helper()
+    if got != want {
+      t.Errorf("got %d want %d given, %v", got, want, arr)
+    }
   }
+
+  t.Run("collection of 5 numbers", func(t *testing.T) {
+    arr := [5]int{1, 2, 3, 4, 5}
+    got := Sum(arr)
+    want := 15
+    assert(t, got, want, arr)
+  })
+
+  t.Run("collection of any size", func(t *testing.T){
+    arr := []int{1, 2, 3}
+    got := Sum(arr)
+    want := 6
+    assert(t, got, want, arr)
+  })
 }
