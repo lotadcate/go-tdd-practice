1b23dac66290a4183c80caff8ad596b326acfb01
lotadcate <lotadcate@gmail.com>
2022-12-18 22:15:23 +0900

    You can define a method by creating a type called Bitcoin.

--- pointerserrors/wallet.go
+++ pointerserrors/wallet.go
@@ -1,15 +1,24 @@
 package pointerserrors
 import "fmt"
 
-type Wallet struct {
-  balance int
+type Stringer interface {
+  String() string
 }
 
-func(w *Wallet) Deposit(amount int) {
+type Bitcoin int
+
+func(b Bitcoin) String() string {
+  return fmt.Sprintf("%d BTC", b)
+}
+
+type Wallet struct {
+  balance Bitcoin
+}
+
+func(w *Wallet) Deposit(amount Bitcoin) {
   w.balance += amount 
 }
 
-func(w *Wallet) Balance() int {
-  fmt.Printf("address of balance in test is %v \n", &w.balance)
+func(w *Wallet) Balance() Bitcoin {
   return w.balance
 }
