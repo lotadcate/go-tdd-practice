e603f312fa2ca3372cbc5445bee3576cca10860c
lotadcate <lotadcate@gmail.com>
2022-12-18 13:57:18 +0900

    cannot use circle (type Circle) as type Rectangle in argument to Area

--- structsmethodsinterfaces/perimeter_test.go
+++ structsmethodsinterfaces/perimeter_test.go
@@ -11,10 +11,24 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-  rectangle := Rectangle{12.0, 6.0}
-  got := Area(rectangle)
-  want := 72.0
-  if got != want {
-    t.Errorf("got %.2f want %.2f", got, want)
-  }  
+  assert := func(t testing.TB, got, want float64) {
+    t.Helper()
+    if got != want {
+      t.Errorf("got %g want %g", got, want)
+    }
+  }
+
+  t.Run("rectangles", func(t *testing.T) {
+    rectangle := Rectangle{12.0, 6.0}
+    got := Area(rectangle)
+    want := 72.0
+    assert(t, got, want)
+  })
+
+  t.Run("circles", func(t *testing.T) {
+    circle := Circle{10}
+    got := Area(circle)
+    want := 314.1592653589793
+    assert(t, got, want)
+  })
 }
