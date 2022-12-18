4d43235aa3926fc0965b1f8d2d5081e79395eabf
lotadcate <lotadcate@gmail.com>
2022-12-18 14:01:03 +0900

    Now try to change to a method.

--- structsmethodsinterfaces/perimeter_test.go
+++ structsmethodsinterfaces/perimeter_test.go
@@ -20,14 +20,14 @@ func TestArea(t *testing.T) {
 
   t.Run("rectangles", func(t *testing.T) {
     rectangle := Rectangle{12.0, 6.0}
-    got := Area(rectangle)
+    got := rectangle.Area()
     want := 72.0
     assert(t, got, want)
   })
 
   t.Run("circles", func(t *testing.T) {
     circle := Circle{10}
-    got := Area(circle)
+    got := circle.Area()
     want := 314.1592653589793
     assert(t, got, want)
   })
