0e15f61bbfc6dc6f8c5bdc63afe9e677f71d0b4f
lotadcate <lotadcate@gmail.com>
2022-12-18 14:08:16 +0900

    As implemented by the method.

--- structsmethodsinterfaces/perimeter.go
+++ structsmethodsinterfaces/perimeter.go
@@ -1,14 +1,24 @@
 package structsmethodsinterfaces
+import "math"
 
 type Rectangle struct {
   Width float64
   Height float64  
 }
 
+func (r Rectangle) Area() float64 {
+  return r.Width * r.Height 
+}
+
 type Circle struct {
   Radius float64
 }
 
+func (c Circle) Area() float64 {
+  return math.Pi * c.Radius * c.Radius
+}
+
+// Just a function.
 func Perimeter(rectangle Rectangle) float64 {
   return 2 * (rectangle.Width + rectangle.Height)
 }
