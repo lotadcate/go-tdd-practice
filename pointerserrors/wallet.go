f82149c47423ae5258e75a4784b3de0fe2b4e093
lotadcate <lotadcate@gmail.com>
2022-12-18 21:59:43 +0900

    The balance is not reflected.

---
+++ pointerserrors/wallet.go
@@ -0,0 +1,15 @@
+package pointerserrors
+import "fmt"
+
+type Wallet struct {
+  balance int
+}
+
+func(w Wallet) Deposit(amount int) {
+  w.balance += amount 
+}
+
+func(w Wallet) Balance() int {
+  fmt.Printf("address of balance in test is %v \n", &w.balance)
+  return w.balance
+}
