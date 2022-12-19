cc5adee7b209fdeb1c93ea436dbf4d083a978335
lotadcate <lotadcate@gmail.com>
2022-12-19 14:27:00 +0900

    Add function body and test added.

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -6,7 +6,11 @@ import (
 
 type Dictionary map[string]string
 
-var ErrNotFound = errors.New("could not find the word you were looking for")
+var (
+  ErrNotFound   = errors.New("could not find the word you were looking for")
+  ErrWordExists = errors.New("cannot add word because it already exists")
+)
+
 func(dict Dictionary) Search(key string) (string, error) {
   // Map can return two values. The first value is value, and the second value is a boolean value indicating whether the key was successfully detected.
   definition, ok := dict[key]
@@ -16,4 +20,19 @@ func(dict Dictionary) Search(key string) (string, error) {
   }
 
   return definition, nil
+}
+
+func(dict Dictionary) Add(word, definition string) error{
+  _, err := dict.Search(word)
+
+  switch err {
+    case ErrNotFound:
+      dict[word] = definition
+    case nil:
+      return ErrWordExists
+    default:
+      return err
+  }    
+
+  return nil
 }
