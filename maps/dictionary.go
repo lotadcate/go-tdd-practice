9703fa62f6f4f05bf118c852376a0d4bc3804975
lotadcate <lotadcate@gmail.com>
2022-12-19 15:02:24 +0900

    Update function body and test added.

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,15 +1,17 @@
 package dictionary
-import (
-  "errors"
-  // "fmt"
-)
 
 type Dictionary map[string]string
 
-var (
-  ErrNotFound   = errors.New("could not find the word you were looking for")
-  ErrWordExists = errors.New("cannot add word because it already exists")
+type DictionaryErr string
+const (
+  ErrNotFound   = DictionaryErr("could not find the word you were looking for")
+  ErrWordExists = DictionaryErr("cannot add word because it already exists")
+  ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
 )
+
+func (e DictionaryErr) Error() string {
+  return string(e)
+}
 
 func(dict Dictionary) Search(key string) (string, error) {
   // Map can return two values. The first value is value, and the second value is a boolean value indicating whether the key was successfully detected.
@@ -36,3 +38,19 @@ func(dict Dictionary) Add(word, definition string) error{
 
   return nil
 }
+
+func(dict Dictionary) Update(word, definition string) error {
+  _, err := dict.Search(word)
+
+  switch err {
+  case ErrNotFound:
+      return ErrWordDoesNotExist
+  case nil:
+      dict[word] = definition
+  default:
+      return err
+  }
+
+  return nil
+}
+
