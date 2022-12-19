76e6f2d67cd80e536a05f7e22cc1f0d221af4203
lotadcate <lotadcate@gmail.com>
2022-12-19 13:51:29 +0900

    A function for string detection and error detection is defined.

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,7 +1,19 @@
 package dictionary
+import (
+  "errors"
+  // "fmt"
+)
 
 type Dictionary map[string]string
 
-func(dict Dictionary) Search(key string) string {
-  return dict[key]
+var ErrNotFound = errors.New("could not find the word you were looking for")
+func(dict Dictionary) Search(key string) (string, error) {
+  // Map can return two values. The first value is value, and the second value is a boolean value indicating whether the key was successfully detected.
+  definition, ok := dict[key]
+  // fmt.Printf("def: 「%q」 ok : 「%t」\n", definition, ok)
+  if !ok {
+    return "", ErrNotFound
+  }
+
+  return definition, nil
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -1,14 +1,25 @@
 package dictionary
-import "testing"
+import (
+  "testing"
+)
 
 func TestSearch(t *testing.T) {
   dictionary := Dictionary {
     "test": "this is just a test",
   }
 
-  got := dictionary.Search("test")
-  want := "this is just a test"
-  assertStrings(t, got, want)
+  // expect to get "string"
+  t.Run("known world", func(t *testing.T) {
+    got, _  := dictionary.Search("test")
+    want := "this is just a test"
+    assertStrings(t, got, want)
+  })
+
+  // expect to get "error"
+  t.Run("unknown world", func(t *testing.T) {
+    _, got := dictionary.Search("unknown")
+    assertError(t, got, ErrNotFound)
+  })
 }
 
 func assertStrings(t *testing.T, got, want string) {
@@ -16,4 +27,11 @@ func assertStrings(t *testing.T, got, want string) {
   if got != want {
     t.Errorf("got %q want %q", got, want)
   }
+}
+
+func assertError(t *testing.T, got, want error) {
+    t.Helper()
+    if got != want {
+      t.Errorf("got %q want %q", got, want)
+    }
 }
