40427bd00dfdcb7d89d49604d23fbd629c7a63b4
lotadcate <lotadcate@gmail.com>
2022-12-20 13:43:36 +0900

    second test

--- mocking/count.go
+++ mocking/count.go
@@ -3,10 +3,19 @@ import (
   "io"
   "fmt"
   "os"
+  "time"
 )
 
+const finalWord = "Go!"
+const countdownStart = 3
+
 func Countdown(out io.Writer) {
- fmt.Fprint(out, "3") 
+  for i:=countdownStart; i>0; i-- {
+    time.Sleep(1 * time.Second)
+    fmt.Fprintln(out, i)
+  }
+  time.Sleep(1 * time.Second)
+  fmt.Fprint(out, finalWord)
 }
 
 func main() {
