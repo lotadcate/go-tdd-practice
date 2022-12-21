aad873a1aac5a2c41e659b674b7b84c367c90a38
lotadcate <lotadcate@gmail.com>
2022-12-21 23:01:20 +0900

    Difficult to make mocks.

--- mocking/count.go
+++ mocking/count.go
@@ -9,9 +9,19 @@ import (
 const finalWord = "Go!"
 const countdownStart = 3
 
-func Countdown(out io.Writer) {
+type Sleeper interface {
+  Sleep()
+}
+
+type DefaultSleeper struct {}
+
+func (d *DefaultSleeper) Sleep() {
+  time.Sleep(1 * time.Second)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
   for i:=countdownStart; i>0; i-- {
-    time.Sleep(1 * time.Second)
+    sleeper.Sleep()
     fmt.Fprintln(out, i)
   }
   time.Sleep(1 * time.Second)
@@ -19,5 +29,6 @@ func Countdown(out io.Writer) {
 }
 
 func main() {
-  Countdown(os.Stdout)
+  sleeper := &DefaultSleeper{}
+  Countdown(os.Stdout, sleeper)
 }
--- mocking/count_test.go
+++ mocking/count_test.go
@@ -6,7 +6,9 @@ import (
 
 func TestCountdown(t *testing.T) {
   buffer := &bytes.Buffer{}
-  Countdown(buffer)
+  defaultSleeper := &DefaultSleeper{}
+
+  Countdown(buffer, defaultSleeper)
 
   got := buffer.String()
   want := `3
@@ -16,5 +18,9 @@ Go!`
 
   if got != want {
     t.Errorf("got %q want %q", got, want)
+  }
+
+  if spySleeper.Calls != 4 {
+    t.Errorf("not enough calls to sleeper, want 4 got %d", spySleeper.Calls)
   }
 }
