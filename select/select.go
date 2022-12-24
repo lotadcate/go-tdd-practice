11d5392a2ec8d66dfc328e22429a2738084d4e69
lotadcate <lotadcate@gmail.com>
2022-12-25 00:37:10 +0900

    The problem is that this test takes 10 seconds to run, although it successfully returns an error if the Racer takes more than 10 seconds.

--- select/select.go
+++ select/select.go
@@ -1,21 +1,26 @@
 package main
 import (
+  "fmt"
   "time"
   "net/http"
 )
 
-func Racer(a, b string) (winner string) {
-  aDuration := measureResponseTime(a)
-  bDuration := measureResponseTime(b)
-
-  if aDuration < bDuration {
-    return a
+func Racer(a, b string) (winner string, error error) {
+  select {
+  case <-ping(a):
+    return a, nil
+  case <-ping(b):
+    return b, nil
+  case <-time.After(10*time.Second):
+    return "", fmt.Errorf("timed out waiting for %s and %s", a, b)  
   }
-  return b
 }
 
-func measureResponseTime(url string) time.Duration {
-  start := time.Now()
-  http.Get(url)
-  return time.Since(start)
+func ping(url string) chan struct{} {
+  ch := make(chan struct{})
+  go func() {
+    http.Get(url)
+    close(ch)
+  }()
+  return ch
 }
--- select/select_test.go
+++ select/select_test.go
@@ -7,21 +7,19 @@ import (
 )
 
 func TestRacer(t *testing.T) {
-  slowServer := makeDelayedServer(20*time.Millisecond)
-  fastServer := makeDelayedServer(0*time.Millisecond)
+  t.Run("returns an error if a server doesn't respond within 10s", func(t *testing.T) {
+    serverA := makeDelayedServer(11 * time.Second)
+    serverB := makeDelayedServer(12 * time.Second)
 
-  defer slowServer.Close()
-  defer slowServer.Close()
+    defer serverA.Close()
+    defer serverB.Close()
 
-  slowURL := slowServer.URL
-  fastURL := fastServer.URL
+    _, err := Racer(serverA.URL, serverB.URL)
 
-  want := fastURL
-  got := Racer(slowURL, fastURL)
-
-  if got != want {
-    t.Errorf("got %q want %q", got, want)
-  }
+    if err == nil {
+      t.Error("expected an error but didn't get one")
+    }
+  })
 }
 
 func makeDelayedServer(delay time.Duration) *httptest.Server {
