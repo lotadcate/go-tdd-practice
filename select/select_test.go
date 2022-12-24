c58934495b4e58b6e5fbf8ee44e725c29561e020
lotadcate <lotadcate@gmail.com>
2022-12-25 01:12:13 +0900

    The timeout can be set later.

--- select/select_test.go
+++ select/select_test.go
@@ -8,18 +8,36 @@ import (
 
 func TestRacer(t *testing.T) {
   t.Run("returns an error if a server doesn't respond within 10s", func(t *testing.T) {
-    serverA := makeDelayedServer(11 * time.Second)
-    serverB := makeDelayedServer(12 * time.Second)
+    slowServer := makeDelayedServer(20 * time.Millisecond)
+    fastServer := makeDelayedServer(0 * time.Millisecond)
 
-    defer serverA.Close()
-    defer serverB.Close()
+    defer slowServer.Close()
+    defer fastServer.Close()
+    
+    slowURL := slowServer.URL
+    fastURL := fastServer.URL
 
-    _, err := Racer(serverA.URL, serverB.URL)
+    want := fastURL
+    got, err := Racer(slowURL, fastURL)
+
+    if err != nil {
+      t.Fatalf("did not expect an error but got one %v", err)
+    }
+
+    if got != want {
+      t.Errorf("got %q, want %q", got, want)
+    }
+  })
+
+  t.Run("returns an error if a server doesn't respond within 10s", func(t *testing.T) {
+    server := makeDelayedServer(25*time.Millisecond)
+    defer server.Close()
+    _, err := ConfigurableRacer(server.URL, server.URL, 20*time.Millisecond)
 
     if err == nil {
       t.Error("expected an error but didn't get one")
     }
-  })
+  }) 
 }
 
 func makeDelayedServer(delay time.Duration) *httptest.Server {
