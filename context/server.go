dd5b0c6e5f84d4fe8b67752ef8686e88fd623c01
lotadcate <lotadcate@gmail.com>
2022-12-25 21:34:04 +0900

    first test

---
+++ context/server.go
@@ -0,0 +1,15 @@
+package main
+import (
+  "net/http"
+  "fmt"
+)
+
+type Store interface {
+  Fetch() string
+}
+
+func Server(store Store) http.HandlerFunc {
+  return func(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, store.Fetch())
+  }
+}
